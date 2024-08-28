b257b875679618f48b004cc68285b16c6bb2ff80
Fhilipe Monteiro <fhilipemonteiro@hotmail.com>
2024-08-28 00:22:16 -0300

    feat: Add UploadSchema for image uploads

---
+++ src/Schemas/UploadSchema.ts
@@ -0,0 +1,28 @@
+import z from 'zod';
+
+const UploadSchema = z.object({
+  image: z.string({
+    required_error: `field 'image' is required`,
+    invalid_type_error: `field 'image' must be a string`,
+  }).base64({
+    message: `field 'image' must be a valid base64 string`,
+  }),
+
+  customer_code: z.string({
+    required_error: `fiel 'customer_code' is required`,
+    invalid_type_error: `field 'customer_code' must be a string`,
+  }),
+
+  measure_datetime: z.string({
+    required_error: `field 'measure_datetime' is required`,
+    invalid_type_error: `field 'measure_datetime' must be a string`,
+  }).date(),
+  
+  measure_type: z.enum([`WATER`, `GAS`], {
+    required_error: `field 'measure_type' is required`,
+    invalid_type_error: `field 'measure_type' must be a string`,
+    message: `field 'measure_type' must be either 'WATER' or 'GAS'`,
+  }),
+});
+
+export default UploadSchema;
