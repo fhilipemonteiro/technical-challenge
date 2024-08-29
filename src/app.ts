c4f466274a6f607e09206c31e6bc43008697c42f
Fhilipe Monteiro <fhilipemonteiro@hotmail.com>
2024-08-29 02:16:11 -0300

    refactor: update app.ts to use instance MySQL, use Env , and fix path Logger

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,28 @@
 import 'module-alias/register';
 import express from 'express';
-import dotenv from 'dotenv';
-import GeminiRouter from '@Routes/Gemini';
-import Logger from '@Helpers/Logs/Logger';
-import MySQL from '@Configs/Db/MySQL';
-
-dotenv.config();
+import Logger from '@Helpers/Logger';
+import Env from '@Helpers/Env';
+import MySQL from '@Configs/MySQL';
+import GeminiRouter from '@Routes/GeminiRoute';
 
 const app = express();
+const mysql = new MySQL();
 
 app.use(express.json());
-
 app.use(GeminiRouter);
 
 const startServer = async () => {
   try {
-    await MySQL.connect();
-    await MySQL.createTableIfNotExists();
-    app.listen(process.env.PORT, () => {
-      Logger.info(`Server is running on port ${process.env.PORT}`);
+    await mysql.connect();
+    await mysql.createTableIfNotExists();
+
+    app.listen(Env.PORT, () => {
+      Logger.info(`Server is running on port ${Env.PORT}`);
     });
   } catch (error) {
     Logger.error('Server failed to start');
     Logger.error(error);
+    process.exit(1);
   }
 };
 
