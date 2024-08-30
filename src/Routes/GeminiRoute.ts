ab767f37b7d808612f7b8bacbd705c19ce33cbcb
Fhilipe Monteiro <fhilipemonteiro@hotmail.com>
2024-08-30 02:37:33 -0300

    feat: listMeasures

--- src/Routes/GeminiRoute.ts
+++ src/Routes/GeminiRoute.ts
@@ -1,22 +1,24 @@
 import { Request, Response, Router } from 'express';
 import ValidateDataReceived from '@Middlewares/ValidateDataReceived';
 import GeminiController from '@Controllers/GeminiController';
-import { UploadSchema, ConfirmSchema } from '@Schemas/'
+import { UploadSchema, ConfirmSchema, ListMeasuresSchema } from '@Schemas/'
+import { ListMeasuresDTO } from './GeminiDTO';
 
 const router = Router();
 
 const geminiController = new GeminiController();
 
-router.post('/upload', ValidateDataReceived(UploadSchema), async (req: Request, res: Response) => {
+router.post('/upload', ValidateDataReceived(UploadSchema, 'INVALID_DATA'), async (req: Request, res: Response) => {
   await geminiController.createMeasure(req, res);
 });
 
 
-router.patch('/confirm', ValidateDataReceived(ConfirmSchema), async (req, res) => {
+router.patch('/confirm', ValidateDataReceived(ConfirmSchema, 'INVALID_DATA'), async (req: Request, res: Response) => {
   await geminiController.confirmMeasure(req, res);
 });
 
-router.get('/:customer_code/list', (req, res) => {
+router.get('/:customer_code/list', ValidateDataReceived(ListMeasuresSchema, 'INVALID_TYPE'), async (req: Request & ListMeasuresDTO, res: Response) => {
+  await geminiController.listMeasures(req, res);
 });
 
 export default router;
