591767b300596846a40b7bd77b68d6f8fc94f001
Fhilipe Monteiro <fhilipemonteiro@hotmail.com>
2024-08-30 02:36:34 -0300

    feat: add listMeasures method to GeminiController

--- src/Controllers/GeminiController.ts
+++ src/Controllers/GeminiController.ts
@@ -2,14 +2,15 @@ import { Request, Response } from "express";
 import { v4 as uuid } from 'uuid';
 import GeminiRepository from "@Repositories/GeminiRepository";
 import GeminiService from "@Services/GeminiService";
-import { ConfirmDTO, ConfirmResponseDTO, UploadDTO, UploadResponseDTO } from "@Routes/GeminiDTO";
+import { ConfirmDTO, ConfirmResponseDTO, ListMeasuresDTO, ListMeasuresResponseDTO, UploadDTO, UploadResponseDTO } from "@Routes/GeminiDTO";
 import HttpError from "@Helpers/HttpError";
 import HttpResponse from "@Helpers/HttpResponse";
 import logger from "@Helpers/Logger";
 
 interface IGeminiController {
   createMeasure: (req: Request, res: Response) => Promise<Response>;
-  // confirmMeasure: (req: Request, res: Response) => Promise<Response>;
+  confirmMeasure: (req: Request, res: Response) => Promise<Response>;
+  listMeasures: (req: Request & ListMeasuresDTO, res: Response) => Promise<Response>;
 }
 
 class GeminiController implements IGeminiController {
@@ -33,7 +34,7 @@ class GeminiController implements IGeminiController {
       const existsMeasure = await this.geminiRepository.checkExistsMeasureOfThePeriod(customer_code, measure_type, measure_datetime);
 
       if (existsMeasure) {
-        return this.httpError.Duplicate(res, {
+        return this.httpError.Conflict(res, {
           error_code: 'DOUBLE_REPORT',
           error_description: 'Leitura do mês já realizada'
         });
@@ -82,10 +83,50 @@ class GeminiController implements IGeminiController {
         });
       }
 
+      const isConfirmed = await this.geminiRepository.checkMeasurementIsAlreadyConfirmed(measure_uuid);
+
+      if (isConfirmed) {
+        return this.httpError.Conflict(res, {
+          error_code: 'CONFIRMATION_DUPLICATE',
+          error_description: 'Leitura do mês já realizada'
+        });
+      }
+
       const confirmMeasure = await this.geminiRepository.confirmMeasure(measure_uuid, confirmed_value);
 
       const response: ConfirmResponseDTO = {
         success: confirmMeasure
+      };
+
+      return this.httpResponse.Ok(res, response);
+    } catch (error) {
+      logger.error('Erro interno no servidor');
+      logger.error(error);
+      return this.httpError.InternalServerError(res, {
+        error_code: 'INTERNAL_SERVER_ERROR',
+        error_description: 'Erro interno no servidor'
+      });
+    }
+  }
+
+  async listMeasures(req: Request & ListMeasuresDTO, res: Response): Promise<Response> {
+
+    const { customer_code } = req.params;
+    const { measure_type } = req.query;
+
+    try {
+      const measures = await this.geminiRepository.getMeasures(customer_code, measure_type);
+
+      if (!measures.length) {
+        return this.httpError.NotFound(res, {
+          error_code: 'MEASURES_NOT_FOUND',
+          error_description: 'Nenhuma leitura encontrada'
+        });
+      }
+
+      const response: ListMeasuresResponseDTO = {
+        customer_code,
+        measures: measures
       };
 
       return this.httpResponse.Ok(res, response);
